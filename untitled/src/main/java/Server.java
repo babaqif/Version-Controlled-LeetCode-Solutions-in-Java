fba743d26f0549bfc97062605a1f70af0050927c
Michael Lumanga <qylumanga@gmail.com>
2024-07-30 08:59:31 +0200

    Client side socket set up

--- untitled/src/main/java/Server.java
+++ untitled/src/main/java/Server.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            ServerSocket socket = new ServerSocket(3000);
+            ServerSocket serversocket = new ServerSocket(3000);
+            Socket socket = serversocket.accept();
+            System.out.println("Received a connection");
+            socket.close();
         } catch (IOException e) {
             System.out.println("Failed to create socket");
             e.printStackTrace();
